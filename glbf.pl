85601a45e2e7036095092863ff7081e72183eabc
Jacopo <jacopomassa97@gmail.com>
2023-09-22 17:01:06 +0200

    now multiple flows consider the Capacity Allocation

--- glbf.pl
+++ glbf.pl
@@ -16,18 +16,19 @@ placeFlows([], Alloc, Alloc, Out, Out).
 placeFlow(FlowId, Alloc, NewAlloc, (Path, B, Delay)) :-
     flow(FlowId, S, D, PacketSize, BurstSize, BitRate, Budget, Th),
     MinB is Budget - Th, MaxB is Budget + Th,
-    path(S, D, (MinB,MaxB), PacketSize, BurstSize, BitRate, [], (B,Path)),
+    path(S, D, (MinB,MaxB), Alloc, PacketSize, BurstSize, BitRate, [], (B,Path)),
     delay((B,Path), Delay), updateFlows(Path, FlowId), updateCapacities(Path, BitRate, Alloc, NewAlloc).
 
-path(S, D, (MinB,MaxB), PacketSize, BurstSize, BitRate, OldPath, NewPath) :-
+path(S, D, (MinB,MaxB), Alloc, PacketSize, BurstSize, BitRate, OldPath, NewPath) :-
     dif(S, D), link(S, N, TProp, Bandwidth), \+ member(N, OldPath), 
-    node(N, MinNodeBudget), Bandwidth > BitRate,
+    node(N, MinNodeBudget), capacity(S, N, Alloc, Capacity), Bandwidth > Capacity + BitRate,
+    %writeln(Bandwidth), writeln(Capacity), writeln(BitRate),
     transmissionTime(PacketSize, Bandwidth, TTime),
     queuingTime(N, PacketSize, BurstSize, Bandwidth, QTime),
     NewMinB is MinB - MinNodeBudget - TProp - TTime, 
     NewMaxB is MaxB - MinNodeBudget - QTime - TProp - TTime, NewMaxB > 0,
-    path(N, D, (NewMinB,NewMaxB), PacketSize, BurstSize, BitRate, [S|OldPath], NewPath).
-path(D, D, Budget, _, _, _, P, (Budget,FP)) :- reverse([D|P],FP).
+    path(N, D, (NewMinB,NewMaxB), Alloc, PacketSize, BurstSize, BitRate, [S|OldPath], NewPath).
+path(D, D, Budget, _, _, _, _, P, (Budget,FP)) :- reverse([D|P],FP).
 
 /* maximum queuing time is the time taken to serialize the sum of the bursts of all flows
    except for one packet of the current flow */
@@ -36,7 +37,7 @@ queuingTime(N, PacketSize, BurstSize, Bandwidth, QTime) :-
     flowsAt(N, Flows), % dif(Flows, []),
     findall(PB, (member(F,Flows), flow(F,_,_,_,P,B,_,_), PB is P * B ), PBs), sumlist(PBs, Sum),
     QTime is (((BurstSize - 1) * PacketSize) + Sum)/Bandwidth.
-%queuingTime(N, _, _, _, 0) :- flowsAt(N, []).
+% queuingTime(N, _, _, _, 0) :- flowsAt(N, []).
 
 transmissionTime(PacketSize, Bandwidth, TTime) :-
     TTime is PacketSize/Bandwidth.
--- utils.pl
+++ utils.pl
@@ -4,10 +4,13 @@ updateCapacities([N1,N2|Ns], BitRate, OldAlloc, NewAlloc) :-
 updateCapacities([N1,N2|Ns], BitRate, OldAlloc, NewAlloc) :-
     \+ member((N1,N2,_), OldAlloc),
     updateCapacities([N2|Ns], BitRate, [(N1,N2,BitRate)|OldAlloc], NewAlloc).
-updateCapacities([_], _, AllocBW, AllocBW).
+updateCapacities([_], _, Alloc, Alloc).
 
 updateFlows([N|Ns], FlowId) :- flowsAt(N, Flows), retract(flowsAt(N, Flows)), assert(flowsAt(N, [FlowId|Flows])), updateFlows(Ns, FlowId).
 updateFlows([], _).
+
+capacity(N1, N2, Alloc, Capacity) :- member((N1,N2,Capacity), Alloc).
+capacity(N1, N2, Alloc, 0) :- \+ member((N1,N2,_), Alloc).
 
 delay(((PathMinB, _),Path), Delay) :- PathMinB > 0, length(Path, L), Hops is L-1, Delay is PathMinB/Hops.
 delay(((PathMinB, _),_), 0) :- PathMinB < 0.
